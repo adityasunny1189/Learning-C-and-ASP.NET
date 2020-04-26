73f3ece5e1607c413b64ff1cee90dd98bf7acaea
adityasunny1189 <adityapathak1189@gmail.com>
2020-04-26 20:31:38 +0530

    day 1

---
+++ gradebook/src/GradeBook/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int x = 23;
+            int y = 45;
+            int ans = x + y;
+            Console.WriteLine($"The sum of {x} and {y} is: {ans}");
+            if(args.Length > 0) {
+                Console.WriteLine("Hello " + args[0] + "!");
+            } else {
+                Console.WriteLine("Hello");
+            }
+        }
+    }
+}
