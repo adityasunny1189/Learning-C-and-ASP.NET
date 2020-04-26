55134ccac96efa2cafcf7b3335bbeaff64f9b5bd
adityasunny1189 <adityapathak1189@gmail.com>
2020-04-27 01:50:12 +0530

    arrays and foreach loop

--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
@@ -7,9 +8,25 @@ namespace GradeBook
         static void Main(string[] args)
         {
 
+            var grade = new List<int>() {20, 22, 21, 24, 25};
+            int finalGrade = 0;
+            grade.Add(20);
+            foreach(var number in grade) {
+                finalGrade += number;
+            }
+            Console.WriteLine($"Final grade total is: {finalGrade}");
+            double avgGrade = finalGrade / grade.Count;
+            Console.WriteLine($"Avg grade is: {avgGrade}");
+            
             int x = 23;
             int y = 45;
             int ans = x + y;
+            int result = 0;
+            int[] arr = new int[] {2, 3, 4};
+            foreach(int num in arr) {
+                result += num;
+            }
+            Console.WriteLine(result);
             Console.WriteLine($"The sum of {x} and {y} is: {ans}");
             if(args.Length > 0) {
                 Console.WriteLine("Hello " + args[0] + "!");
