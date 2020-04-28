384b0387a38ac71ad6ff7a270014cf822e2e356e
adityasunny1189 <adityapathak1189@gmail.com>
2020-04-28 11:09:14 +0530

    pass by ref and get output

---
+++ PassingPara/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PassingPara
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            getValue(out a, out b);
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            passByRef(ref a, ref b);
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+
+        static void getValue(out int a, out int b) {
+            a = 5;
+            b = 10;
+        }
+
+        static void passByRef(ref int a, ref int b) {
+            a++;
+            b++;
+        }
+    }
+}
