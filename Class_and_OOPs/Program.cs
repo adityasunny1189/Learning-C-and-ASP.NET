f00e3eae71ff047b1e7e82edf99fdc542c979dd4
adityasunny1189 <adityapathak1189@gmail.com>
2020-04-28 11:33:59 +0530

    classes and oops

---
+++ Class_and_OOPs/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Class_and_OOPs
+{
+    public class Student {
+        public string name;
+        public int age;
+        public int semester;
+        public int cgpa;
+
+        public Student(string name, int age, int sem, int cgpa) {
+            this.name = name;
+            this.age = age;
+            this.semester = sem;
+            this.cgpa = cgpa;
+        }
+
+        public void print() {
+            Console.WriteLine($"Name : {this.name} Age: {this.age} Semester: {this.semester} CGPA: {this.cgpa}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var S1 = new Student("Aditya", 19, 4, 8);
+            var S2 = new Student("Amit", 20, 4, 9);
+            S1.print();
+            S2.print();
+        }
+    }
+}
